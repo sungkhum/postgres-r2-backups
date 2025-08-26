79b24b69e25f7cc45b02fa7c0601d88f0040795f
Nathan <sungkhum@gmail.com>
2025-08-26 07:12:47 -0700

    UPdate

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,33 @@
-import { CronJob } from "cron"
-import { backup } from "./backup.js"
-import { env } from "./env.js"
+import { CronJob } from "cron";
+import { backup } from "./backup.js";
+import { env } from "./env.js";
 
-console.log("NodeJS Version: " + process.version)
+console.log("NodeJS Version: " + process.version);
 
 const tryBackup = async () => {
   try {
-    await backup()
+    await backup();
   } catch (error) {
-    console.error("Error while running backup: ", error)
-    process.exit(1)
+    console.error("Error while running backup: ", error);
+    process.exit(1);
   }
 }
 
 if (env.RUN_ON_STARTUP || env.SINGLE_SHOT_MODE) {
-  console.log("Running on start backup...")
+  console.log("Running on start backup...");
 
-  await tryBackup()
+  await tryBackup();
 
   if (env.SINGLE_SHOT_MODE) {
-    console.log("Database backup complete, exiting...")
-    process.exit(0)
+    console.log("Database backup complete, exiting...");
+    process.exit(0);
   }
 }
 
 const job = new CronJob(env.BACKUP_CRON_SCHEDULE, async () => {
-  await tryBackup()
-})
+  await tryBackup();
+});
 
-job.start()
+job.start();
 
-console.log("Backup cron scheduled...")
+console.log("Backup cron scheduled...");
